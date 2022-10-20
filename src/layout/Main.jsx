c375710dc9ea4cd77ea78f02bedcaca506a8dd2e
Timur Tarazov <johnsyplay@yandex.ru>
2022-10-20 14:04:15 +0500

    Movies added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,4 +1,25 @@
-function Main() {
-    return <main className="content container"></main>
+import React from "react";
+import {Movies} from "../components/Movies_list";
+
+class Main extends React.Component {
+    state = {
+        movies: []
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=bc223a58&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}));
+    }
+
+    render() {
+        return <main className="content container">
+            {
+                this.state.movies.length ? (<Movies movies={this.state.movies} />) : <h5>Loading...</h5>
+            }
+        </main>
+
+    }
+
 }
 export {Main}
