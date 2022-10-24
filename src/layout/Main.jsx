97bea957f7c1c6f20f546c597ae524f2e91d64df
Timur Tarazov <johnsyplay@yandex.ru>
2022-10-24 18:28:52 +0500

    search refactored

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Movies} from "../components/Movies_list";
 import {Preloader} from '../components/Preloader';
-import {Search} from '../components/Search';
+import Search from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
